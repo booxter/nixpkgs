604ee9f7669979ed8310d410ba1a8dd48bb8a8c6
Ihar Hrachyshka <ihar.hrachyshka@gmail.com>
2026-07-31 14:51:49 -0400

    swiftPackages.test-swift-repl: use one-line declaration

--- pkgs/development/compilers/swiftPackages/by-name/te/test-swift-repl/package.nix
+++ pkgs/development/compilers/swiftPackages/by-name/te/test-swift-repl/package.nix
@@ -13,9 +13,7 @@ runCommand "test-swift-repl"
   ''
     export LLDB_DEBUGSERVER_PATH=/Applications/Xcode.app/Contents/SharedFrameworks/LLDB.framework/Resources/debugserver
     cat <<EOF | swift repl | grep "Saying: Hello, Nixpkgs!"
-      func say(message: String) {
-        print("Saying: \(message)")
-      }
+      func say(message: String) { print("Saying: \(message)") }
       say(message: "Hello, Nixpkgs!")
     EOF
     touch "$out"
