3692fe63c183d50cf4468b8bf766ceded4674461
Randy Eckenrode <randy@largeandhighquality.com>
2026-08-09 08:38:12 -0400

    llvmPackages_{18,19,20,21,22}.libllvm: backport Darwin triple parsing
    This fixes breakage on triples for macOS 27, which aligned the Darwin
    version with the OS version (darwin26 is macOS 27, but so is darwin27,
    which will be incorrectly interpreted as macOS 28 without this patch).
    This is probably harmless in most cases, but it causes a test failure in
    LLVM’s test suite when running under macOS 27.
    
    (cherry picked from commit 6fb7a4dc0c9ba446ba0968bd046601a2e5903571)

--- pkgs/development/compilers/llvm/common/llvm/default.nix
+++ pkgs/development/compilers/llvm/common/llvm/default.nix
@@ -222,7 +222,14 @@ stdenv.mkDerivation (
               hash = "sha256-3hkbYPUVRAtWpo5qBmc2jLZLivURMx8T0GQomvNZesc=";
               stripLen = 1;
             }
-          );
+          )
+      ++ lib.optionals (lib.versionOlder release_version "23") [
+        # As of macOS 27 (and iOS 27, etc), the Darwin version number is the same as the OS version number.
+        # This change breaks target parsing because `darwin27` is incorrectly interpreted as macOS 28.
+        # This patch is a backport of the target parsing changes in LLVM 23, which fixes the problem.
+        # Hopefully, Apple does not change the version number scheme again any time soon.
+        (getVersionFile "llvm/backport-darwin-triple-parsing.patch")
+      ];
 
     nativeBuildInputs = [
       cmake
